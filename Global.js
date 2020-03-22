24e07da9752143fbb43aa22199f6f4b7bafbf96c
3sani <62345744+3sani@users.noreply.github.com>
2020-03-22 05:32:03 +0200

    Document & clean up global variables

--- Global.js
+++ Global.js
@@ -1,30 +1,79 @@
+/**
+ * @type {Number}
+ * Mathematical constant tau = 2 * pi.
+ */
 const tau = 2 * Math.PI;
 
-/** @type {number} Number of HTML5 canvas frames are drawn before the synthesisation loops */
+/**
+ * @type {number}
+ * Number of HTML5 canvas frames before the synthesisation loops.
+ */
 var synthesisPeriod = 360;
 
-/** @type {number} Counter for HTML5 canvas frames  */
+/**
+ * @type {number}
+ * Framecounter for HTML5 canvas frames.
+ */
 var p = 0;
 
-// TODO add all of these as parameters into the app
-var fadeColor = "rgb(0,0,0)";
+/**
+ * @type {Number}
+ * How much the previous frame is faded out on the synthesisation canvas.
+ */
 var fadeOpacity = 0.5;
+
+/**
+ * @type {String}
+ * What colour previous frames on the synthesisation canvas are faded with.
+ */
+var fadeColor = "rgb(0,0,0)";
+
+/**
+ * @type {String}
+ * The page background color.
+ */
 var bgColor = "black";
 
+/**
+ * @type {String}
+ * The colour waves are drawn in on the oscillator visualisers.
+ */
 var waveColor = "rgb(85, 255, 33)";
 
-/** @type {number} Center x-coordinate for the browser window */
+/**
+ * @type {number}
+ * Center x-coordinate for the browser window.
+ */
 var centreX;
 
-/** @type {number} Center x-coordinate for the browser window */
+/**
+ * @type {number}
+ * Center x-coordinate for the browser window.
+ */
 var centreY;
 
-/** @type {Object[]} Array containing every active oscillator */
+/**
+ * @type {Object[]}
+ * Array containing every active oscillator.
+ */
 var oscillators = [];
 
 // TODO Add multiple canvases to allow for layered effects
-/** @type {Object} Reference to the HTML5 canvas used for the main synthesis */
+/**
+ * @type {Object}
+ * Reference to the HTML5 canvas used for the main synthesis.
+ */
 var sCanvas = document.querySelector('#synth');
 
-/** @type {Object} Reference to the main synthesis canvas graphics context. Used for drawing onto the canvas.*/
+/**
+ * @type {Object} 
+ * Reference to the main synthesis canvas graphics context.
+ * Used for drawing onto the canvas.
+ */
 var s = sCanvas.getContext('2d');
+
+/** 
+ * @type {Number}
+ * Number of oscillators before the "add oscillator" button gets hidden.
+ */
+var maxOscillators = 10;
