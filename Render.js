2eab05fc52432ded0c006efec3d5e775ab737155
3sani <62345744+3sani@users.noreply.github.com>
2020-04-20 01:37:29 +0300

    Restructure for instrument based drawing

--- Render.js
+++ Render.js
@@ -1,62 +1,18 @@
-$(document).ready(function () {
-    sCanvas.width = window.innerWidth;
-    sCanvas.height = window.innerHeight;
-
-    document.body.style.backgroundColor = bgColor;
-    centreX = innerWidth / 2;
-    centreY = innerHeight / 2;
-
-    Oscillator.addOscillator();
-
-    // TODO REMOVE. Adding second oscillator here just for debugging.
-    Oscillator.addOscillator();
-
-    // TODO DEBUGGING INSTRUMENT, PLEASE REMOVE
-    debugInstrument = new Circle(10);
-    ///debugCircle = debugInstrument.getImageData();
-
-    animate();
-});
-
-
-window.onresize = function () {
-    sCanvas.width = window.innerWidth;
-    sCanvas.height = window.innerHeight;
-    centreX = innerWidth / 2;
-    centreY = innerHeight / 2;
-}
-
-// The main loop for the whole app. Called as fast as the browser pleases.
+// The main loop for the whole app.
 function animate() {
     p++;
     if (p >= synthesisPeriod) {
         p = 0;
     }
 
-    //#region placeholder instruments for debugging
-    // s.globalAlpha = fadeOpacity;
-    // s.fillStyle = fadeColor;
-    // s.fillRect(0, 0, innerWidth, innerHeight);
-    // s.globalAlpha = 1;
-    // s.strokeStyle = "rgb(85, 255, 33)";
-    // s.beginPath();
-    // s.arc(
-    //     centreX + oscillators[0].val[p] * 0.475 * innerWidth,
-    //     centreY + oscillators[1].val[p] * 0.475 * innerHeight,
-    //     20, 0, 2 * Math.PI);
-    // s.stroke();
-    //#endregion
-
     s.globalAlpha = fadeOpacity;
     s.fillStyle = fadeColor;
     s.fillRect(0, 0, innerWidth, innerHeight);
     s.globalAlpha = 1;
 
-    debugInstrument.drawInstrument(
-        s, 
-        centreX + oscillators[0].val[p] * 0.475 * innerWidth,
-        centreY + oscillators[1].val[p] * 0.475 * innerHeight,
-    )
+    instruments.forEach(instrument => {
+        if(instrument.enabled) instrument.drawInstrument(s,p)
+    });
 
     requestAnimationFrame(animate);
 }
